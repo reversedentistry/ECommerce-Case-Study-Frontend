aad01b0500094449e0aeeaccb9d5c05e755881c0
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-04-27 14:39:27 -0700

    Added products map

--- src/components/Shop/Shop.tsx
+++ src/components/Shop/Shop.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import ProductCard from "./ProductCard";
 
 interface Product {
     productId: number,
@@ -8,22 +9,24 @@ interface Product {
 };
 
 function ShopPage() {
-    const [allProducts, setAllProducts] = useState<Product[] | null>(null); 
-        
-    const getProducts = async () => { 
+    const [allProducts, setAllProducts] = useState<Product[] | null>(null);
+
+    const getAllProducts = async () => {
         axios.get("http://localhost:3030/products").then(
             response => {
-                const products:Product[] = response.data;
-                setAllProducts(products); 
+                const products: Product[] = response.data;
+                setAllProducts(products);
             })
             .catch(err => {
                 console.log(err);
-                }
+            }
             );
-    }
-    
+    };
 
- 
+    useEffect(() => {
+        getAllProducts();
+        allProducts?.map((product: Product) => console.log(product));
+    });
 
 
     return (
@@ -33,7 +36,15 @@ function ShopPage() {
                     <a className="button">LOG OUT</a>
                 </div>
             </nav>
-            
+            <section className="container mt-5">
+                <div className="columns">
+                    {allProducts?.map((product: Product) =>
+                        <ProductCard key={product.productId} name={product.productName} price={product.price} />
+                    )}
+                </div>
+
+
+            </section>
         </div>
 
     )
