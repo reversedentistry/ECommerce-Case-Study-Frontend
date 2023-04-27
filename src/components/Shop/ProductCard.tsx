47e742e1d7f5cf3fe6e780f9855cc3fbf66790bc
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-04-27 12:47:51 -0700

    Created individual model for product cards

---
+++ src/components/Shop/ProductCard.tsx
@@ -0,0 +1,19 @@
+interface ProductInfo {
+    name: string,
+    price: number
+}
+
+function ProductCard(product:ProductInfo) {
+    return (
+        <div className="card">
+            <div className="card-content">
+                <div className="content">
+                    <h5>{product.name}</h5>
+                    <h6>{product.price}</h6>
+                </div>
+            </div>
+        </div>
+    )
+}; 
+
+export default ProductCard;
