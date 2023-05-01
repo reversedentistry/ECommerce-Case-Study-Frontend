f3de01c1130e1b82eb4a926ab4751e18c564e3c5
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-05-01 14:55:25 -0700

    Added some more styles and logout function

--- src/components/Shop/Shop.tsx
+++ src/components/Shop/Shop.tsx
@@ -1,15 +1,17 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import ProductCard from "./ProductCard";
+import { Product } from "../../models/Product";
+import { logoutUser } from "../../features/authSlice";
+import { useAppDispatch } from "../../redux/hooks";
+import { useNavigate } from "react-router-dom";
 
-interface Product {
-    productId: number,
-    productName: string,
-    price: number
-};
 
 function ShopPage() {
     const [allProducts, setAllProducts] = useState<Product[] | null>(null);
+
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate(); 
 
     const getAllProducts = async () => {
         axios.get("http://localhost:3030/products").then(
@@ -28,18 +30,29 @@ function ShopPage() {
         allProducts?.map((product: Product) => console.log(product));
     }, []);
 
+    function logout(){
+        dispatch(logoutUser()); 
+        navigate("/login"); 
+    }
+
 
     return (
         <div>
             <nav className="navbar-end">
                 <div className="navbar-item">
-                    <a className="button">LOG OUT</a>
+                    <a className="button mr-2" href="/cart">Cart</a>
+                    <button className="button" onClick={logout}>LOG OUT</button>
                 </div>
             </nav>
+            <section className="hero is-info">
+                <div className="hero-body has-text-centered">
+                    <p className="title">Our products</p>
+                </div>
+            </section>
             <section className="container mt-5">
                 <div className="columns">
                     {allProducts?.map((product: Product) =>
-                        <ProductCard key={product.productId} name={product.productName} price={product.price} />
+                        <ProductCard key={product.productId} productId={product.productId} productName={product.productName} price={product.price} />
                     )}
                 </div>
 
