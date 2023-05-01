68d037d94f2f92613ac7d00c54956a51cc559333
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-05-01 12:19:53 -0700

    Created functions to clear cart and simulate order submission

--- src/components/Shop/Cart.tsx
+++ src/components/Shop/Cart.tsx
@@ -1,12 +1,24 @@
+import { Product } from "../../models/Product";
 import { useEffect, useState } from "react";
 
 function Cart() {
-    const [cart, setCart] = useState<Object[] | null>(null);
+    const [cart, setCart] = useState<Product[] | null>(null);
 
     useEffect(() => {
         let cartArray = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart") || "") : [];
         setCart(cartArray);
-    }, []); 
+    }, []);
+
+    function clearCart() {
+        localStorage.clear(); 
+        setCart(null); 
+    };
+
+    function submitOrder() {
+        localStorage.clear();
+        setCart(null); 
+        alert("Order submitted!");
+    };
 
     return (
         <div>
@@ -16,8 +28,25 @@ function Cart() {
                     <a className="button">LOG OUT</a>
                 </div>
             </nav>
+            <section className="columns is-multiline">
+                {cart?.map(cartItem => {
+                    if (cartItem != null) {
+                        return (
+                            <div key={cartItem.productId} className="column card">
+                                <div className="card-content">
+                                    <div className="content">
+                                        <h5>{cartItem.productName}</h5>
+                                        <h6>{cartItem.price}</h6>
+                                    </div>
+                                </div>
+                            </div>)
+                    }
+                })}
+
+            </section>
             <section>
-                
+                <button onClick={clearCart}>Clear cart</button>
+                <button onClick={submitOrder}>Checkout</button>
             </section>
         </div>
     )
