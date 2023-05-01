a0e1590bc8f15813a303a6fa8215bff7cebcc885
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-05-01 14:54:43 -0700

    Added more routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import "bulma/css/bulma.min.css";
 import RegistrationPage from './components/Registration/Registration';
 import ShopPage from './components/Shop/Shop';
+import LoginPage from './components/Login/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Cart from './components/Shop/Cart';
 
 function App() {
   return (
   <BrowserRouter>
     <Routes>
-      <Route path="register" element={<RegistrationPage />}></Route>
-      <Route path="/" element={<ShopPage />}></Route>
+      <Route path="/" element={<RegistrationPage />}></Route>
+      <Route path="/login" element={<LoginPage/>}></Route>
+      <Route path="shop" element={<ShopPage />}></Route>
+      <Route path="/cart" element={<Cart/>}></Route>
     </Routes>
   </BrowserRouter>
   )
