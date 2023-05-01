f3decb68d33b66135f2259ae10264fc9b82a545d
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-04-30 22:06:07 -0700

    Added function to add product to cart

--- src/components/Shop/ProductCard.tsx
+++ src/components/Shop/ProductCard.tsx
@@ -1,9 +1,23 @@
 interface ProductInfo {
+    id: number,
     name: string,
-    price: number
+    price: number,
 }
 
 function ProductCard(product:ProductInfo) {
+    function addToCart() {
+        let cart = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart") || "") : [];
+
+        if (cart[product.id]) {
+            return;
+        } else {
+            cart[product.id] = product;
+        };
+
+        localStorage.setItem("cart", JSON.stringify(cart));
+
+    }
+    
     return (
         <div className="column">
         <div className="card">
@@ -11,6 +25,7 @@ function ProductCard(product:ProductInfo) {
                 <div className="content">
                     <h5>{product.name}</h5>
                     <h6>{product.price}</h6>
+                    <button onClick={addToCart}>Add to cart</button>
                 </div>
             </div>
         </div>
